83c5ca56563d09131fc567f87d49e280a2642714
Moisés Jurado <moises003@gmail.com>
2023-06-16 15:31:29 -0500

    se importa el archivo .ts en el archivo html

---
+++ bases/app.ts
@@ -0,0 +1,27 @@
+
+
+const msg1 = 'hola mundo'; //pasa el mouse por encima de la variable para ver el tipo
+console.log('msg1')
+console.log(msg1)
+
+let msg2 = 'hola mundo'; //aqui el tipo ya es string
+console.log('msg2')
+console.log(msg2)
+
+msg2 = 'otra cadena'; //con "let" el tipo se mantiene igual
+console.log('msg2')
+console.log(msg2)
+
+msg2 = 123; // y no permite cambiarlo, en ejecucion. Dará error de "compilacion" en el editor de texto.
+console.log('msg2')
+console.log(msg2)
+//*mientras typeScript no esté importado tampoco afectará e igual funcionará en runtime
+
+//tambien le puedo definir tipo a la constante
+//pero esto es solo de typescript asi q javascript no lo conoce y se cae en runtime
+//Uncaught SyntaxError: Missing initializer in const declaration
+const msg3 = 'hola mundo';
+console.log('msg3')
+console.log(msg3)
+//si comentas el ": string" funcionará normal.
+
